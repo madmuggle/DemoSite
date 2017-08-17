6d3ff6474b4cfcc859288ecaad3b880ac8ccbadd
WangXing <wang.xing@e-ports.com>
2017-08-17 17:57:12 +0800

    sql executing with right error handling

--- server/lib/services/createuser.js
+++ server/lib/services/createuser.js
@@ -7,10 +7,13 @@ async function handle(req) {
   req.data.registertime = mkTimeString(new Date());
   const { fields, values } = mkInsertString(req.data);
   const sqlStr = `INSERT INTO users(${fields}) VALUES(${values})`;
-  await db.executeSql(sqlStr);
-  return {
-    status: "success",
-  };
+  try {
+    await db.executeSql(sqlStr);
+    return { status: "success", };
+  } catch (e) {
+    logger.error("SQL error: ", e.code);
+    return { status: "failed" };
+  }
 }
 
 
